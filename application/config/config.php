6fa1185f0860e30a4c45242a523f1c1da2b4449f
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-05-18 18:03:12 +0530

    config update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://dumkalcollege.org/NEWERP/';
+$config['base_url'] = 'http://localhost/NEWERP_DUMKAL';
 
 
 
